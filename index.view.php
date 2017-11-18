c8c23e6eaa318c89a94c0e91425c8c44613f6164
erkin <bokushev@gmail.com>
2017-11-18 18:00:55 +0200

    init

---
+++ index.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach ($tasks as $task):?>
+        <li><?= $task->description?></li>
+        <?php endforeach;?>
+    </ul>
+    
+</body>
+</html>
