657bb1b4717ae6e53c1770fee2f026c79c06be5a
erkin27 <bokushev@gmail.com>
2017-11-18 23:05:36 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 return [
     'database' => [
         'name' => 'mytodo',
-        'username' => 'erkin',
-        'password' => 'Qwerty123',
+        'username' => '',
+        'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
