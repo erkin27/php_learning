232743ae2c37522a47dd0f802c3e5ab6637fab5a
erkin <bokushev@gmail.com>
2017-11-19 23:44:29 +0200

    composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,5 @@
 <?php
 
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
-require 'Router.php';
-require 'Request.php';
-
 $app = [];
 
 $app['config'] = require __DIR__.'/../config.php';
